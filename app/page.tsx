8c8b5743b07cdd7041ab1145a6eec4491e7c0551
Isac Alves <isac_na@hotmail.com>
2025-04-07 20:19:43 -0300

    Add analytics

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { HeroSection } from "@/components/hero-section"
 import { ServicesSection } from "@/components/services-section"
 import { AboutSection } from "@/components/about-section"
 import { ContactSection } from "@/components/contact-section"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
