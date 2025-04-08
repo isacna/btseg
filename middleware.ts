c5d93d96ce853e517f926f323a57e430d262b455
Isac Alves <isac_na@hotmail.com>
2025-04-07 23:00:34 -0300

    Debug route

--- middleware.ts
+++ middleware.ts
@@ -38,7 +38,7 @@ export function middleware(request: NextRequest) {
 
     if (origin && allowedOrigin && !origin.includes(allowedOrigin)) {
       return new NextResponse(
-        JSON.stringify({ error: `Invalid origin: ${origin}` }),
+        JSON.stringify({ error: `Invalid origin: ${origin} & ${allowedOrigin}` }),
         { status: 403, headers: { 'Content-Type': 'application/json' } }
       )
     }
